239f59e05ef803067986bde5fd750eb55d362221
ErixKim <edkim@NN123-HA-22.HS.sd203.org>
2015-10-27 14:12:51 -0500

    Chapter 6 Algorithms
    Algorithm practice

---
+++ Chapter 6 practice/findMaxMin.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class findMaxMin
+{
+    public static double findMax()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a series of number or a character to quit. ");
+
+        double largest = scan.nextDouble();
+        while( scan.hasNextDouble())
+        {
+            double input = scan.nextDouble();
+            if (input > largest)
+            {
+                largest = input;
+            }
+        }
+        return largest;
+    }
+    public static double findMin()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a series of number or a character to quit. ");
+        
+        double smallest = scan.nextDouble();
+        while( scan.hasNextDouble())
+        {
+            double input = scan.nextDouble();
+            if (input < smallest)
+            {
+                smallest = input;
+            }
+        }
+        return smallest;
+    }
+}
+
