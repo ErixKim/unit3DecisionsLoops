6a25b7629e3d5532bf441c39ca2334ed141f837a
ErixKim <edkim@NN123-HA-22.HS.sd203.org>
2015-11-02 14:12:53 -0600

    Diamond
    11/2

--- Chapter 6 practice/Diamond.java
+++ Chapter 6 practice/Diamond.java
@@ -3,5 +3,22 @@ public class Diamond
 {
     public static void filledDiamond()
     {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What number: ");
+        int number = scan.nextInt();
+        int count = 0;
+        int i = 1;
+        while (count <= number)
+        {
+            while ( i <= number)
+            {
+              System.out.println("*");
+              i += 1;
+            }
+            
+            System.out.println("\n");
+            count ++;
+        }
         
     }
+}
