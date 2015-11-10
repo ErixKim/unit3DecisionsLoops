0448f0d1d4b0bf3eb1cd2bcd4533e5955397398e
ErixKim <edkim@NN123-HA-22.HS.sd203.org>
2015-11-10 14:12:36 -0600

    GameofLife
    11/10

--- GameOfLifeLab/GameOfLife.java
+++ GameOfLifeLab/GameOfLife.java
@@ -17,9 +17,9 @@ public class GameOfLife
     // the world comprised of the grid that displays the graphics for the game
     private ActorWorld world;
     
-    // the game board will have 6 rows and 6 columns
-    private final int ROWS = 6;
-    private final int COLS = 6;
+    // the game board will have 10 rows and 10 columns
+    private final int ROWS = 10;
+    private final int COLS = 10;
     
     /**
      * Default constructor for objects of class GameOfLife
@@ -68,39 +68,39 @@ public class GameOfLife
         
         // create and add rocks (a type of Actor) to the three intial locations
         Rock rock1 = new Rock();
-        Location loc1 = new Location(Y1, X1);
+        Location loc1 = new Location(X1 , Y1);
         grid.put(loc1, rock1);
         
         Rock rock2 = new Rock();
-        Location loc2 = new Location(Y2, X2);
+        Location loc2 = new Location(X2 , Y2);
         grid.put(loc2, rock2);
         
         Rock rock3 = new Rock();
-        Location loc3 = new Location(Y3, X3);
+        Location loc3 = new Location(X3 , Y3);
         grid.put(loc3, rock3);
         
         Rock rock4 = new Rock();
-        Location loc4 = new Location(Y4, X4);
+        Location loc4 = new Location(X4 , Y4);
         grid.put(loc4, rock4);
         
         Rock rock5 = new Rock();
-        Location loc5 = new Location(Y5, X5);
+        Location loc5 = new Location(X5 , Y5);
         grid.put(loc5, rock5);
        
         Rock rock6 = new Rock();
-        Location loc6 = new Location(Y6, X6);
+        Location loc6 = new Location(X6 , Y6);
         grid.put(loc6, rock6);
         
         Rock rock7 = new Rock();
-        Location loc7 = new Location(Y7, X7);
+        Location loc7 = new Location(X7 , Y7);
         grid.put(loc7, rock7);
         
         Rock rock8 = new Rock();
-        Location loc8 = new Location(Y8, X8);
+        Location loc8 = new Location(X8 , Y8);
         grid.put(loc8, rock8);
         
         Rock rock9 = new Rock();
-        Location loc9 = new Location(Y9, X9);
+        Location loc9 = new Location(X9 , Y9);
         grid.put(loc9, rock9);
     }
 
@@ -122,7 +122,10 @@ public class GameOfLife
         Grid<Actor> grid = world.getGrid();
         
         // insert magic here...
-        
+        while (count <= 100)
+        {
+            
+        }
     }
     
     /**
--- GameOfLifeLab/GameOfLifeTest.java
+++ GameOfLifeLab/GameOfLifeTest.java
@@ -45,16 +45,20 @@ public class GameOfLifeTest
         /* expected pattern for initial state
          *  (X: alive; -: dead)
          * 
-         *    0 1 2 3 4 5 6
-         *  0 - - - - - - -
-         *  1 - X - - X - -
-         *  2 - - - - - X -
-         *  3 - X - - - X -
-         *  4 - - X X X X -
-         *  5 - - - - - - -
-         *  6 - - - - - - -
-         */
-        
+         *    0 1 2 3 4 5 6 7 8 9 10
+         *  0 - - - - - - - - - - - 
+         *  1 - X - - X - - - - - - 
+         *  2 - - - - - X - - - - -
+         *  3 - X - - - X - - - - -
+         *  4 - - X X X X - - - - -
+         *  5 - - - - - - - - - - -
+         *  6 - - - - - - - - - - -
+         *  7 - - - - - - - - - - -
+         *  8 - - - - - - - - - - -
+         *  9 - - - - - - - - - - -
+         *  10- - - - - - - - - - - 
+         */ 
+
         GameOfLife game = new GameOfLife();
         final int ROWS = game.getNumRows();
         final int COLS = game.getNumCols();
@@ -68,14 +72,14 @@ public class GameOfLifeTest
 
                 // if the cell at the current row and col should be alive, assert that the actor is not null
                 if(     (row == 1 && col == 1) ||
-                        (row == 3 && col == 1) ||
-                        (row == 4 && col == 2) ||
-                        (row == 4 && col == 3) ||
-                        (row == 4 && col == 4) ||
-                        (row == 4 && col == 5) ||
-                        (row == 3 && col == 5) ||
-                        (row == 2 && col == 5) ||
-                        (row == 1 && col == 4)
+                (row == 3 && col == 1) ||
+                (row == 4 && col == 2) ||
+                (row == 4 && col == 3) ||
+                (row == 4 && col == 4) ||
+                (row == 4 && col == 5) ||
+                (row == 3 && col == 5) ||
+                (row == 2 && col == 5) ||
+                (row == 1 && col == 4)
                 )
                 {
                     assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
@@ -93,8 +97,56 @@ public class GameOfLifeTest
     {
         /* verify that the actual pattern matches the expected pattern after 3 generations         *  
          */
-        
-        // ...
+        /* expected pattern for final state
+         *  (X: alive; -: dead)
+         * 
+         *    0 1 2 3 4 5 6 7 8 9 10
+         *  0 - - - - - - - - - - - 
+         *  1 - - - - - - - - - - - 
+         *  2 - - - X X X X - - - -
+         *  3 - - X - - - X - - - -
+         *  4 - - - - - - X - - - -
+         *  5 - - X - - X - - - - -
+         *  6 - - - - - - - - - - -
+         *  7 - - - - - - - - - - -
+         *  8 - - - - - - - - - - -
+         *  9 - - - - - - - - - - -
+         *  10- - - - - - - - - - - 
+         */ 
+
+        GameOfLife game = new GameOfLife();
+        final int ROWS = game.getNumRows();
+        final int COLS = game.getNumCols();
+
+        for(int row = 0; row < ROWS; row++)
+        {
+            for(int col = 0; col < COLS; col++)
+            {
+                // in this example, an alive cell has a non-null actor and a dead cell has a null actor
+                Actor cell = game.getActor(row, col);
+
+                // if the cell at the current row and col should be alive, assert that the actor is not null
+                if(     (row == 2 && col == 3) ||
+                (row == 2 && col == 4) ||
+                (row == 2 && col == 5) ||
+                (row == 2 && col == 6) ||
+                (row == 3 && col == 6) ||
+                (row == 3 && col == 2) ||
+                (row == 4 && col == 6) ||
+                (row == 5 && col == 2) ||
+                (row == 5 && col == 5)
+                )
+                {
+                    assertNotNull("expected alive cell at (" + row + ", " + col + ")", cell);
+                }
+                else // else, the cell should be dead; assert that the actor is null
+                {
+                    assertNull("expected dead cell at (" + row + ", " + col + ")", cell);
+                }
+            }
+
+            // ...
+        }
     }
 }
 
